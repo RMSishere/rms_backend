9a3eba8806c72ed48969b6e029642755cb6da6c4
Muhammad Umair <umairinam76@gmail.com>
2025-05-30 02:01:01 +0500

    Added new data

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -213,8 +213,8 @@ export class SubscriptionController {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
-      success_url: `${body.success_url}?leadId=${body.leadId}`,
-      cancel_url: `${body.cancel_url}?leadId=${body.leadId}`,
+      success_url: `${body.success_url}`,
+      cancel_url: `${body.cancel_url}`,
       metadata: {
         userId: user.id.toString(),
         leadId: body.leadId,
