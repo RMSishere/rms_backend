81343d0f5b45874785d7bea8179cdea166b55aff
umairinam <umairinam76@gmail.com>
2025-06-24 15:41:08 +0500

    Your message

--- src/lib/base.dto.ts
+++ src/lib/base.dto.ts
@@ -1,15 +1,15 @@
-import { Base } from './base';
-
 export class BaseDto implements Base {
-    constructor(base: Base) {
-        this._id = base._id;
-        this.id = base.id;
-        this.createdAt = base.createdAt;
-        this.updatedAt = base.updatedAt;
-        this.updatedBy = base.updatedBy;
-        this.createdBy = base.createdBy;
-
+    constructor(base?: Base | null) {
+        if (base) {
+            this._id = base._id;
+            this.id = base.id;
+            this.createdAt = base.createdAt;
+            this.updatedAt = base.updatedAt;
+            this.updatedBy = base.updatedBy;
+            this.createdBy = base.createdBy;
+        }
     }
+
     _id?: string;
     id?: string;
     createdAt?: Date;
