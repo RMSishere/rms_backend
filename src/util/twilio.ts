b891959b995e74c844ec4e01b0673e5cef741e67
umairinam <umairinam76@gmail.com>
2025-06-28 18:41:58 +0500

    Your message

--- src/util/twilio.ts
+++ src/util/twilio.ts
@@ -1,56 +1,41 @@
 import twilio = require('twilio');
 
-const twilioClient = twilio(
+const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN,
 );
 
-export const twilioVerifyService = twilioClient.verify.services(
+export const twilioVerifyService = client.verify.services(
   process.env.TWILIO_VERIFICATION_SERVICE_SID,
 );
-export const twilioNotifyService = twilioClient.notify.services(
+export const twilioNotifyService = client.notify.services(
   process.env.TWILIO_NOTIFY_SERVICE_A,
 );
 
 export const sendBulkTextMessage = async (
   body: string,
-  numbersList: string[] = [],
+  numbersList: string[] = []
 ): Promise<any> => {
   try {
-    if (!numbersList.length) {
-      return;
-    }
+    if (!numbersList.length) return;
 
-    const chunk = 10000;
-    const arrayOfBindings = [];
-    const bindings = numbersList
+    const fromNumber = process.env.TWILIO_PHONE_NUMBER;
+
+    const promises = numbersList
       .filter(number => !!number)
-      .map(number => {
-        return JSON.stringify({ binding_type: 'sms', address: number });
-      });
-
-    for (let i = 0, j = bindings.length; i < j; i += chunk) {
-      arrayOfBindings.push(bindings.slice(i, i + chunk));
-    }
-
-    const promises = [];
-
-    for (let i = 0; i < arrayOfBindings.length; i++) {
-      const bindings = arrayOfBindings[i];
-      promises.push(
-        twilioNotifyService.notifications.create({ toBinding: bindings, body }),
+      .map(number =>
+        client.messages.create({
+          body,
+          to: number,
+          from: fromNumber,
+        })
       );
-    }
 
-    let res;
-    if (promises.length) {
-      res = await Promise.all(promises);
-    }
-
-    return res;
+    const responses = await Promise.all(promises);
+    console.log(responses);
+    return responses;
   } catch (err) {
     console.error(err);
-
     throw err;
   }
 };
