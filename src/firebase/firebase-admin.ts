f5e9d3ddb0b3ee6df05add94b5e68915bd72a46a
umairinam <umairinam76@gmail.com>
2025-12-12 06:11:17 +0500

    Added new data

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,7 +1,20 @@
+// src/firebase/firebase-admin.ts
 import * as admin from 'firebase-admin';
 import * as path from 'path';
+import * as fs from 'fs';
 
-const serviceAccount = require(path.join(__dirname, 'firebase', 'runmysale-b68ad-firebase-adminsdk-fbsvc-ec5b0f8416.json'));
+// Resolve JSON path ALWAYS from project root
+const serviceAccountPath = path.resolve(
+  __dirname,
+  '../../../src/firebase/runmysale-b68ad-firebase-adminsdk-fbsvc-ec5b0f8416.json'
+);
+
+if (!fs.existsSync(serviceAccountPath)) {
+  console.error('❌ Firebase JSON file not found at:', serviceAccountPath);
+  throw new Error('Firebase service account JSON missing');
+}
+
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
