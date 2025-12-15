05ed9c0b33a40cd820916dc2d126294780883809
umairinam <umairinam76@gmail.com>
2025-12-16 00:44:01 +0500

    Fix:Replaced the pit

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
-
-// Initialize Firebase Admin SDK using the credentials
 const serviceAccount = require('./runmysale-b68ad-firebase-adminsdk-fbsvc-ec5b0f8416.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// Only initialize if not already initialized
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
